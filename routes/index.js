5694f80ff1421f0af1ea1886729db41c09ba8d81
mohanaveluk <mohanaveluk@gmail.com>
2019-10-16 07:46:16 -0500

    adding post pdf file

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const pdfkit = require('pdfkit');
+const pdfDoc = require('pdfkit');
 const fs = require('fs');
 var router = express.Router();
 
@@ -18,6 +18,21 @@ router.get('/createPdf', (req, res, next) => {
   console.log(`Create pdf clicked`);
   doc.pipe(fs.createWriteStream('file.pdf')); // write to PDF
   doc.font('Times-Roman').fontSize(48).text('Node js to create pdf files');
+  doc.pipe(res); 
+  doc.end();
+});
+
+router.post('/saveAsPdf', (req, res, next) => {
+  const doc = new pdfDoc;
+  let fileName = req.body.filename;
+  let content = req.body.content;
+
+  res.setHeader('content-disposition', 'attachment; filename="' + fileName + '"');
+  res.setHeader('content-type','application/pdf');
+
+  console.log(`Create pdf clicked`);
+  //doc.pipe(fs.createWriteStream('file.pdf')); // write to PDF
+  //doc.font('Times-Roman').fontSize(48).text('Node js to create pdf files');
   doc.pipe(res); 
   doc.end();
 });
